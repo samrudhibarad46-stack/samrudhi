140fc7155c307b4342e5e987c7f163b6873d422f
samrudhibarad46-stack <samrudhibarad46@gmail.com>
2026-01-06 08:29:25 +0530

    Add files via upload

---
+++ second.py
@@ -0,0 +1,28 @@
+print("hello")
+x="hello world"
+print(x)
+print(20)
+x="20.5"
+print(x)
+x=1j
+print(x)
+x=["apple","banana","cherry"]
+print(x)
+x=("apple","banana","cherry")
+print(x)
+x=range(6)
+print(x)
+x={"name":"samrudhi","age":36}
+print(x)
+x=frozenset({"apple","banana","cherry"})
+print(x)
+x=True
+print(x)
+x= b"hello"
+print(x)
+x=bytearray(5)
+print(x)
+x=memoryview(bytes(5))
+print(x)
+x=None
+print(x)
