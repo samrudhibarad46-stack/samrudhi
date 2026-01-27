fb8586d858efb0250c3c6bd2338ea267bd8cf84f
samrudhibarad46-stack <samrudhibarad46@gmail.com>
2026-01-27 08:16:43 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,57 @@
+# 1 arithmetic
+a=5
+b=10
+print(a+b)
+print(a-b)
+print(a/b)
+print(a*b)
+
+# 2 assignment
+
+x=10
+print(x)
+
+# 3unary minus
+
+a=5
+print(-a)
+
+# 4 relational 
+
+a=10
+b=5
+print(a>b)
+print(a==b)
+
+# 5 boolean 
+
+is_pass=True
+print(is_pass)
+
+# 6bitwise
+
+a=5
+b=3
+print(a&b)
+print(a/b)
+
+# 7 membership
+
+numbers=[1,2,3,4,5]
+print(2 in numbers)
+print(5 not in numbers)
+
+# 8 identity
+
+x=10
+y=10
+print(x is y)
+print(x is not y)
+
+#logical 
+
+x=True
+y=False
+print(x and y)
+print(x or y)
+print(not x)
