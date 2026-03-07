a5aa219258432db140a6b68d776ea5f4012b71b3
samrudhibarad46-stack <samrudhibarad46@gmail.com>
2026-03-07 08:31:18 +0530

    Add files via upload

---
+++ while.py
@@ -0,0 +1,19 @@
+i=1
+while i<=10:
+ print(i)
+ i=i+1
+# 2 
+n=int(input("Enter n:"))
+i=1
+s=0
+while i<=n:
+        s=s+i
+        i=i+1
+        print("sum=",s) 
+
+# 3
+num=int(input("enter number:"))
+i=1
+while i<=10:
+    print(num,"x",i,"=",num*i)
+    i=i+1
