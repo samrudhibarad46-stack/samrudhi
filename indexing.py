9ecb35405842d4025b0ecb54669b2157ae62f572
samrudhibarad46-stack <samrudhibarad46@gmail.com>
2026-03-07 08:20:46 +0530

    Add files via upload

---
+++ indexing.py
@@ -0,0 +1,19 @@
+from array import array
+arr=array('i',[10,20,30,40,50])
+print(arr[0])
+print(arr[2])
+print(arr[4])
+
+arr=array('i',[10,20,30,40,50])
+print(arr[-1])
+print(arr[-2])
+print(arr[-5])
+
+arr=array('i',[10,20,30,40,50])
+arr[2]=35
+print(arr)
+
+arr=array('i',[10,20,30,40,50,60])
+print(arr[5])
+
+
