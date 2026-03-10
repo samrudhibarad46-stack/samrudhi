90320e1e36a1e6124e42a0493eb7c37691734f3e
samrudhibarad46-stack <samrudhibarad46@gmail.com>
2026-03-10 08:18:39 +0530

    Add files via upload

---
+++ slices.py
@@ -0,0 +1,19 @@
+from array import array
+arr=array('i',[10,20,30,40,50])
+
+print(arr[1:4])
+print(arr[:3])
+print(arr[2:])
+print(arr[:])
+
+print(arr[::2])
+print(arr[1::2])
+print(arr[::3])
+
+print(arr[-4:-1])
+print(arr[-3:1])
+print(arr[:-2])
+
+print(arr[::-1])
+
+print(arr[1:4])
