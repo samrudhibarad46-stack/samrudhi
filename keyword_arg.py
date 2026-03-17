60bc86d2591c72832a3a0b7795f40337aab41430
samrudhibarad46-stack <samrudhibarad46@gmail.com>
2026-03-17 08:32:17 +0530

    Add files via upload

---
+++ keyword_arg.py
@@ -0,0 +1,18 @@
+def student_info(name,age,city):
+    print("name:",name)
+    print("age:",age)
+    print("city:",city)
+
+student_info(age=18,city="rajkot",name="samrudhi")    
+
+def display(a,b,c):
+    print("a=",a)
+    print("b=",b)
+    print("c=",c)
+display(1,c=3,b=4)    
+
+def simple_interest(p,r,n):
+    si=(p*r*n)/100
+    print("simple inetrest:",si)
+simple_interest(10000,2,2)
+simple_interest(50000,1,2)
