29219707b7f70c7f3369594ebc698a7fc2ee5211
samrudhibarad <samrudhibarad46@gmail.com>
2026-04-09 17:22:15 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,21 @@
+try:
+    a = float(input("Enter first number: "))
+    b = float(input("Enter second number: "))
+    op = input("Enter operator (+, -, *, /): ")
+
+    if op == "+":
+        print("Result:", a + b)
+    elif op == "-":
+        print("Result:", a - b)
+    elif op == "*":
+        print("Result:", a * b)
+    elif op == "/":
+        print("Result:", a / b)
+    else:
+        print("Invalid operator!")
+
+except Exception as e:
+    print("Error:", e)
+
+finally:
+    print("Calculator finished.")
